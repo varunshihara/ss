b5fdba60c8298e24d0620a730f45a5b273978d94
Varun Shihara <varunshihara@gmail.com>
2014-10-18 09:17:54 +0530

    Update 404.php
    Replacing localhost with 127.0.0.1 - (faster loading)

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -1,4 +1,4 @@
 <hr>
 <h1>It looks like you lost somewhere.</h1>
-<h5><a href="localhost">click here</a> to go to SteelShoppers home page.</h5>
+<h5><a href="127.0.0.1">click here</a> to go to SteelShoppers home page.</h5>
 <hr>
