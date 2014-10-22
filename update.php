0fe68a1c0dfca32e38d2f0d05c92e201c5922351
varunshihara <varunshihara@gmail.com>
2014-10-22 13:14:47 +0530

    update.php
    Allowing user to Update their data

---
+++ update.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once 'header.php';
+
+if(!$user->isLoggedIn()) {
+    Redirect::to('index.php');
+}
+
+if(Input::exists()) {
+    if(Token::check(Input::get('token'))) {
+
+        $validate = new Validate();
+        $validation = $validate->check($_POST, array(
+            'name' => array(
+                'required' => true,
+                'min' => 2,
+                'max' => 50
+            )
+        ));
+
+        if($validation->passed()) {
+
+            try {
+                $user->update(array(
+                    'name' => Input::get('name')
+                ));
+
+                Session::flash('update', 'Your details have been updated.');
+                Redirect::to('update.php');
+
+            } catch(Exception $e) {
+                die($e->getMessage());
+            }
+
+        } else {
+            foreach($validation->errors() as $error) {
+                echo $error . '<br>';
+            }
+        }
+    }
+}
+?>
+
+<div class="row">
+
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+        <?php
+        if(Session::exists('update')) {
+            echo '<div class="alert alert-success" role="alert">' . Session::flash('update') . '</div><br>';
+        }
+        ?>
+
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Update Info</h3>
+            </div>
+            <div class="panel-body">
+                <form action="" method="post" class="form-group">
+                    <label class="form-group">Name :<input type="text" name="name" value="<?php echo escape($user->data()->name); ?>" class="form-control"></label>
+                    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-3"></div>
+</div>
