11753b56c0ca32cb2d91f4f6908eec4ff00a5550
varunshihara <varunshihara@gmail.com>
2014-10-22 13:12:15 +0530

    404
    Update 404 Page

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -1,4 +1,15 @@
-<hr>
-<h1>It looks like you lost somewhere.</h1>
-<h5><a href="127.0.0.1">click here</a> to go to SteelShoppers home page.</h5>
-<hr>
+
+
+<div class="row">
+
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+
+        <hr>
+        <h1>It looks like you lost somewhere.</h1>
+        <h5><a href="http://127.0.0.1/ss/">click here</a> to go to SteelShoppers home page.</h5>
+        <hr>
+
+    </div>
+    <div class="col-md-2"></div>
+</div>
